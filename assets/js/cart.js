b78292571a7a640d29a4fb950312456ca5b08008
ashtonw1 <awalke37@ashland.edu>
2024-12-04 22:39:26 -0500

    Add files via upload

---
+++ assets/js/cart.js
@@ -0,0 +1,5 @@
+function addCart (){
+    var itemName=document.getElementById("name").innerHTML;
+    var itemValue=document.getElementById("price").innerHTML;
+    alert(itemName+" ("+itemValue+")"+" added to Cart");
+}
